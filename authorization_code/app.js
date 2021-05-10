00dca4c532b711291296c4ea1ab905698710df99
Tanmay Siwach <77550145+the-auk@users.noreply.github.com>
2021-05-09 17:52:00 -0700

    Add files via upload

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -1,27 +1,22 @@
-/**
- * This is an example of a basic node.js script that performs
- * the Authorization Code oAuth2 flow to authenticate against
- * the Spotify Accounts.
- *
- * For more information, read
- * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
- */
 
-var express = require('express'); // Express web server framework
-var request = require('request'); // "Request" library
+var express = require('express'); 
+var request = require('request'); 
 var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = '3054cb711c3c4fc48cff3458cdaddea2'; // Your client id
-var client_secret = 'b43bf5c8fe0040829f0029ac301a5ea7'; // Your secret
-var redirect_uri = 'http://localhost:8888/callback/'; // Your redirect uri
+var client_id = '3054cb711c3c4fc48cff3458cdaddea2'; 
+var client_secret = 'b43bf5c8fe0040829f0029ac301a5ea7';
+var redirect_uri = 'http://localhost:8888/callback/';
+var usersid='';
+var tokenuse='';
+var SpotifyWebApi = require('spotify-web-api-node');
+var spotifyApi = new SpotifyWebApi({
+  clientId: '3054cb711c3c4fc48cff3458cdaddea2',
+  clientSecret: 'b43bf5c8fe0040829f0029ac301a5ea7',
+  redirectUri: 'http://www.example.com/callback'
+});
 
-/**
- * Generates a random string containing numbers and letters
- * @param  {number} length The length of the string
- * @return {string} The generated string
- */
 var generateRandomString = function(length) {
   var text = '';
   var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -41,12 +36,13 @@ app.use(express.static(__dirname + '/public'))
    .use(cookieParser());
 
 app.get('/login', function(req, res) {
-
+  console.log('login');
   var state = generateRandomString(16);
   res.cookie(stateKey, state);
 
   // your application requests authorization
   var scope = 'user-read-private user-read-email';
+
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
@@ -58,9 +54,6 @@ app.get('/login', function(req, res) {
 });
 
 app.get('/callback', function(req, res) {
-
-  // your application requests refresh and access tokens
-  // after checking the state parameter
 
   var code = req.query.code || null;
   var state = req.query.state || null;
@@ -91,6 +84,8 @@ app.get('/callback', function(req, res) {
 
         var access_token = body.access_token,
             refresh_token = body.refresh_token;
+        tokenuse = access_token;
+        spotifyApi.setAccessToken(access_token);
 
         var options = {
           url: 'https://api.spotify.com/v1/me',
@@ -101,6 +96,8 @@ app.get('/callback', function(req, res) {
         // use the access token to access the Spotify Web API
         request.get(options, function(error, response, body) {
           console.log(body);
+          userid = body.id;
+          usersid = body.id;
         });
 
         // we can also pass the token to the browser to make requests from there
@@ -120,8 +117,6 @@ app.get('/callback', function(req, res) {
 });
 
 app.get('/refresh_token', function(req, res) {
-
-  // requesting access token from refresh token
   var refresh_token = req.query.refresh_token;
   var authOptions = {
     url: 'https://accounts.spotify.com/api/token',
@@ -136,6 +131,7 @@ app.get('/refresh_token', function(req, res) {
   request.post(authOptions, function(error, response, body) {
     if (!error && response.statusCode === 200) {
       var access_token = body.access_token;
+      token = body.access_token;
       res.send({
         'access_token': access_token
       });
@@ -143,5 +139,44 @@ app.get('/refresh_token', function(req, res) {
   });
 });
 
+var songs = ''
+app.get('/playlists', function(req,res){
+  spotifyApi.getUserPlaylists(usersid)
+  .then(function(data) {
+    console.log('Retrieved playlists', data.body);
+    res.send(data.body);
+    for (let playlist of data.body.items) {
+      console.log(playlist.name + " " + playlist.id)
+      
+      let tracks = getPlaylistTracks(playlist.id, playlist.name);
+  
+      const tracksJSON = { tracks }
+      let data = JSON.stringify(tracksJSON);
+      console.log(data);
+    }
+  },function(err) {
+    console.log('Something went wrong!', err);
+  });
+
+  async function getPlaylistTracks(playlistId, playlistName) {
+
+    const data = await spotifyApi.getPlaylistTracks(playlistId, {
+      offset: 0,
+      limit: 100,
+      fields: 'items'
+    })
+  
+    let tracks = [];
+  
+    for (let track_obj of data.body.items) {
+      const track = track_obj.track
+      tracks.push(track);
+      console.log(track.name + " : " + track.artists[0].name)
+    }
+    
+    console.log("---------------+++++++++++++++++++++++++")
+    return tracks;
+  }
+});
 console.log('Listening on 8888');
 app.listen(8888);
